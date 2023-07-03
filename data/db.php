cd397c8c1e2cd857bd8b7a1cef149378c73dc7e0
Zakaria96 <zakarialavoro@gmail.com>
2023-07-03 16:54:59 +0200

    added details

--- data/db.php
+++ data/db.php
@@ -4,8 +4,13 @@
     require_once("./models/Category.php");
 
     $prod1 = new Product("Crocchette per cani", 10.99, "Cibo per cani di tutte le razze", "Cani");
-    $prod2 = new Product("Snack per cani", 15.00, "Biscotti e ossi per cani di tutte le razze", "Cani");
-    $prod3 = new Product("Snack per gatti", 10.99, "Snack per gatti", "Gatti");
+    $prod2 = new Product("Snack per cani", 15.00, "Biscotti per cani di tutte le razze", "Cani");
+    $prod3 = new Product("Snack per gatti", 21.99, "Snack per gatti", "Gatti");
+    $prod4 = new Product("Crocchette per cani", 16.30, "Cibo per cani di tutte le razze", "Cani");
+    $prod5 = new Product("Snack per cani", 14.00, "Ossi per cani di tutte le razze", "Cani");
+    $prod6 = new Product("Cibo per gatti", 12.99, "Cibo per gatti", "Gatti");
+    $prod7 = new Product("Snack per cani", 15.00, "Biscotti per cani di tutte le razze", "Cani");
+    $prod8 = new Product("Snack per gatti", 21.99, "Snack per gatti", "Gatti");
     
     $categoryG = new Category("Gatti");
     $categoryC = new Category("Cani");
--- index.php
+++ index.php
@@ -10,41 +10,117 @@
     
     <title> <?php echo $category -> getNameCategory(); ?></title>
 </head>
-<body>
-    <div class="container_fluid">
-        <h1 class="text-center" ><?php echo $category -> getNameCategory(); ?></h1>
-        <div class="container d-flex justify-content-between">
-            <div class="card">
-                <div class="card-img">
-                    <img src="./img/Cane.jpg" alt="">
+<body class="bg-dark">
+    <div class="container_fluid ">
+        <h1 class="text-center" style="color: white;"><?php echo $category -> getNameCategory(); ?></h1>
+        <div class="container ">
+            <div class="row"> 
+                <div class="col-3">
+                    <div class="card p-2">
+                        <div class="card-img-top">
+                            <img src="./img/prodotti.jpg" alt="">
+                        </div>
+                        <div class="card_body">
+                            <h5 class="card-title"><?php echo $prod1->getName(); ?></h5>
+                            <h6 class="card-subtitle mb-2 text-muted"><?php echo $categoryC -> getNameCategory(); ?></h6>
+                            <p class="card-text"><?php echo $prod1->getDescription(); ?></p>
+                            <p class="card-text">Prezzo: <?php echo $prod1->getPrice(); ?></p>
+                        </div>
+                    </div>
                 </div>
-                <div class="card_body">
-                    <h5 class="card-title"><?php echo $prod1->getName(); ?></h5>
-                    <h6 class="card-subtitle mb-2 text-muted"><?php echo $categoryC -> getNameCategory(); ?></h6>
-                    <p class="card-text"><?php echo $prod1->getDescription(); ?></p>
-                    <p class="card-text">Prezzo: <?php echo $prod1->getPrice(); ?></p>
+                <div class="col-3">
+                    <div class="card p-2">
+                        <div class="card-img-top">
+                            <img src="./img/prodotti.jpg" alt="">
+                        </div>
+                        <div class="card_body">
+                            <h5 class="card-title"><?php echo $prod2->getName(); ?></h5>
+                            <h6 class="card-subtitle mb-2 text-muted"><?php echo $categoryC ->getNameCategory(); ?></h6>
+                            <p class="card-text"><?php echo $prod2->getDescription(); ?></p>
+                            <p class="card-text">Prezzo: <?php echo $prod2->getPrice(); ?></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card p-2">
+                        <div class="card-img-top">
+                            <img src="./img/prodotti.jpg" alt="">
+                        </div>
+                        <div class="card_body">
+                            <h5 class="card-title"><?php echo $prod3->getName(); ?></h5>
+                            <h6 class="card-subtitle mb-2 text-muted"><?php echo $categoryG ->getNameCategory(); ?></h6>
+                            <p class="card-text"><?php echo $prod3->getDescription(); ?></p>
+                            <p class="card-text">Prezzo: <?php echo $prod3->getPrice(); ?></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card p-2">
+                        <div class="card-img-top">
+                            <img src="./img/prodotti.jpg" alt="">
+                        </div>
+                        <div class="card_body">
+                            <h5 class="card-title"><?php echo $prod4->getName(); ?></h5>
+                            <h6 class="card-subtitle mb-2 text-muted"><?php echo $categoryG ->getNameCategory(); ?></h6>
+                            <p class="card-text"><?php echo $prod4->getDescription(); ?></p>
+                            <p class="card-text">Prezzo: <?php echo $prod4->getPrice(); ?></p>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="card">
-            <div class="card-img">
-                    <img src="./img/Cane.jpg" alt="">
+
+            <div class="row py-3"> 
+                <div class="col-3">
+                    <div class="card p-2">
+                        <div class="card-img-top">
+                            <img src="./img/prodotti.jpg" alt="">
+                        </div>
+                        <div class="card_body">
+                            <h5 class="card-title"><?php echo $prod5->getName(); ?></h5>
+                            <h6 class="card-subtitle mb-2 text-muted"><?php echo $categoryC -> getNameCategory(); ?></h6>
+                            <p class="card-text"><?php echo $prod5->getDescription(); ?></p>
+                            <p class="card-text">Prezzo: <?php echo $prod5->getPrice(); ?></p>
+                        </div>
+                    </div>
                 </div>
-                <div class="card_body">
-                    <h5 class="card-title"><?php echo $prod2->getName(); ?></h5>
-                    <h6 class="card-subtitle mb-2 text-muted"><?php echo $categoryC ->getNameCategory(); ?></h6>
-                    <p class="card-text"><?php echo $prod2->getDescription(); ?></p>
-                    <p class="card-text">Prezzo: <?php echo $prod2->getPrice(); ?></p>
+                <div class="col-3">
+                    <div class="card p-2">
+                        <div class="card-img-top">
+                            <img src="./img/prodotti.jpg" alt="">
+                        </div>
+                        <div class="card_body">
+                            <h5 class="card-title"><?php echo $prod6->getName(); ?></h5>
+                            <h6 class="card-subtitle mb-2 text-muted"><?php echo $categoryC ->getNameCategory(); ?></h6>
+                            <p class="card-text"><?php echo $prod6->getDescription(); ?></p>
+                            <p class="card-text">Prezzo: <?php echo $prod6->getPrice(); ?></p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="card">
-            <div class="card-img">
-                    <img src="./img/Gatto.jpg" alt="">
+                <div class="col-3">
+                    <div class="card p-2">
+                        <div class="card-img-top">
+                            <img src="./img/prodotti.jpg" alt="">
+                        </div>
+                        <div class="card_body">
+                            <h5 class="card-title"><?php echo $prod7->getName(); ?></h5>
+                            <h6 class="card-subtitle mb-2 text-muted"><?php echo $categoryG ->getNameCategory(); ?></h6>
+                            <p class="card-text"><?php echo $prod7->getDescription(); ?></p>
+                            <p class="card-text">Prezzo: <?php echo $prod7->getPrice(); ?></p>
+                        </div>
+                    </div>
                 </div>
-                <div class="card_body">
-                    <h5 class="card-title"><?php echo $prod3->getName(); ?></h5>
-                    <h6 class="card-subtitle mb-2 text-muted"><?php echo $categoryG ->getNameCategory(); ?></h6>
-                    <p class="card-text"><?php echo $prod3->getDescription(); ?></p>
-                    <p class="card-text">Prezzo: <?php echo $prod3->getPrice(); ?></p>
+                <div class="col-3">
+                    <div class="card p-2">
+                        <div class="card-img-top">
+                            <img src="./img/prodotti.jpg" alt="">
+                        </div>
+                        <div class="card_body">
+                            <h5 class="card-title"><?php echo $prod8->getName(); ?></h5>
+                            <h6 class="card-subtitle mb-2 text-muted"><?php echo $categoryG ->getNameCategory(); ?></h6>
+                            <p class="card-text"><?php echo $prod8->getDescription(); ?></p>
+                            <p class="card-text">Prezzo: <?php echo $prod8->getPrice(); ?></p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
